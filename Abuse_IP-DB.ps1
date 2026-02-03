836b7d1522707a0a4ec333ffe0d5b52893207506
Alb1noM00s3 <bparkinson.infosec@gmail.com>
2026-02-03 14:18:47 -0500

    Add files via upload

---
+++ Abuse_IP-DB.ps1
@@ -0,0 +1,10 @@
+﻿Write-Host "This Script will check the IP you supply against AbuseIPDB"
+
+$pythonPath = "C:\Users\parkinsonb\AppData\Local\Programs\Python\Python313\python.exe"
+$ScriptPath = "C:\Python\AbuseIPdb.py"
+$arg = Read-Host "Please Enter an IP to Check"
+$Output = & $pythonPath $scriptpath $arg
+
+$Output
+
+Read-Host "Press Enter to Exit"
